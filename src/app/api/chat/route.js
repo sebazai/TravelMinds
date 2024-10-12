71f80c3b4993c89a958a2449f94369ecf7db9198
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2024-10-12 12:59:37 +0300

    Change system prompt

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -8,7 +8,7 @@ export async function POST(req) {
 
   const result = await streamText({
     model: llama("openhermes2.5-mistral"),
-    system: `You are a helpful assistant. Your only job is to provide a Google Nearby Search API URL as an answer to the user's question. My location is latitude ${data.location.latitude}, and longitude ${data.location.longitude}.`,
+    system: `Your task is to gather information until you can provide a Google Nearby API URL as an answer. However, if the user has not provided sufficient information to fulfill the API request, please ask questions until you are able provide an answer. The user's location is latitude ${data.location.latitude}, and longitude ${data.location.longitude}. The conversation should not contain the user's location or should not reveal your intentions. Never ask for the user's city, town or location as it is provided to you.`,
     messages,
   });
 

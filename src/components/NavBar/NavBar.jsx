e88364a83a800c19b0d768bb76830f4550995d39
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2024-10-21 11:36:26 +0800

    Fix layout

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,30 +1,29 @@
-import {BottomNavigation, BottomNavigationAction, Paper} from "@mui/material";
+import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import SettingsIcon from "@mui/icons-material/Settings";
-import {useState} from "react";
-import {useRouter} from "next/navigation";
-const routes = ['/favorites', '/map', '/settings']
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+const routes = ["/favorites", "/map", "/settings"];
 
-export const NavBar = ( ) => {
+export const NavBar = () => {
   const [value, setValue] = useState(1);
   const router = useRouter();
   return (
-    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-
-    <BottomNavigation
-    showLabels
-    value={value}
-    onChange={(event, newValue) => {routes
-      router.push(routes[newValue]);
-      setValue(newValue);
-    }}
-  >
-    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-    <BottomNavigationAction label="POIs" icon={<LocationOnIcon />} />
-    <BottomNavigationAction label="Settings" icon={<SettingsIcon />} />
-  </BottomNavigation>
-    </Paper>
-
-      )
-}
+    <div style={{ marginTop: "auto", width: "100vw" }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          routes;
+          router.push(routes[newValue]);
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="POIs" icon={<LocationOnIcon />} />
+        <BottomNavigationAction label="Settings" icon={<SettingsIcon />} />
+      </BottomNavigation>
+    </div>
+  );
+};

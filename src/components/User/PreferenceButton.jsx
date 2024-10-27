871ba9b498d687b387e64e923709cc35e9daa6b4
Raphael Boutin <raphael.boutin@etu.u-bordeaux.fr>
2024-10-27 10:53:45 +0100

    change

--- src/components/User/PreferenceButton.jsx
+++ src/components/User/PreferenceButton.jsx
@@ -2,10 +2,14 @@
 import React from 'react';
 import { Button, Typography, Box } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add'; 
+import {useRouter} from "next/navigation";
+
 
 const AddNewPreferenceButton = () => {
+  const router = useRouter();
+
   const handleClick = () => {
-    window.location.href = '/new_preference';
+    router.push('/new_preference');
   };
 
   return (

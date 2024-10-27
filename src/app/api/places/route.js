8f7f0eff5e522d3dc4578a6637f51aa7d61645b1
Sebastian <39335537+sebazai@users.noreply.github.com>
2024-10-27 14:30:02 +0200

    Update src/app/api/places/route.js
    Co-authored-by: Mikita Pilinka <mikita.pilinka@northern.tech>

--- src/app/api/places/route.js
+++ src/app/api/places/route.js
@@ -9,7 +9,7 @@ export async function POST(req) {
 
   const systemPrompt = `You are a helpful tour guide.
 
-    Your task is to find relevant information about places based on user input. Limit the search close to the user's location and to a maximum of five places.
+    Your task is to find relevant places to visit based on user preferences expressed in user input . Limit the search close to the user's location and to a maximum of five places.
     The location of the user is latitude ${data.location[0]} and longitude ${data.location[1]}.
 
     The output for each place should include keys "address", "name", and "coordinates". "coordinates" is an object with keys "latitude" and "longitude".

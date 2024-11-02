10720e4d332e694c7f40891e24a025f386a94b51
Mikita Pilinka <mikita.pilinka@northern.tech>
2024-11-02 12:40:25 +0200

    preferences are fetched from the server

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,15 +1,16 @@
 import { connectToDataBase } from "@/app/lib/db";
-import { User } from '@/models/User';
+import { User } from "@/models/User";
 import { Preference } from "@/models/Preference";
-import { NextResponse } from 'next/server';
-import { Favorite } from "@/models/Favorite"; 
+import { NextResponse } from "next/server";
+import { Favorite } from "@/models/Favorite";
 import { Preference } from "@/models/Preference";
-
 
 export async function GET() {
   try {
     await connectToDataBase();
-    const users = await User.findOne().populate('preferences').populate('favorites');
+    const users = await User.findOne()
+      .populate("preferences")
+      .populate("favorites");
     return NextResponse.json(users);
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
